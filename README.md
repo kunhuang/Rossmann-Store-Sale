9532b141cc7742674dd18eadc861f33dd72f2812
kradnangel <zhouqianren.dark@gmail.com>
2015-11-19 01:19:31 -0800

    Add the data file and start.py
    The a2_start.py includes reading data and writing data.
    predict.csv is the output file used to submit.

--- README.md
+++ README.md
@@ -17,3 +17,41 @@
 |3|11.29| |
 |2|11.30| Write the report|
 |1|12.01| Deadline|
+
+
+## Data Format
+
+The data is a [{}].
+
+Every list represents a line in data file.
+In every list, you can use data[i]['Sales'] to choose the attribute you want.
+
+
+**Example of data[i]:**
+
+{'DayOfWeek': 5, 'Customers': 625, 'StateHoliday': '0', 'Promo': 1, 'Sales': 6064, 'Date': '2015-07-31', 'Open': 1, 'SchoolHoliday': '1', 'Store': 2}
+
+'Store' is int
+
+'DayOfWeek' is int
+
+'Date' is string
+
+'Sales' is int
+
+'Customers' is int 
+
+'Open' is int
+
+'StateHoliday' is string
+
+'SchoolHoliday' is string
+
+
+
+## NOTICE
+There are some mistake in data file (test.csv). I have found the solution in the official forums ([here](https://www.kaggle.com/c/rossmann-store-sales/forums/t/16835/open-is-blank-in-test-file-for-store-622)) and I have fixed it. Please use the data files in the Github.
+
+
+## Reference
+[How to read and write .csv in python](https://docs.python.org/2/library/csv.html)
---
+++ a2_start.py
@@ -0,0 +1,71 @@
+import math
+import csv
+
+### read .csv file and return a list of list, a line in file is a list.
+def readCSV(filename):
+    data = []
+    with open(filename, 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        data = map(lambda row: (''.join(row)).split(','), spamreader)
+    print "Reading Done!"
+    return data
+
+### preprocess the train data
+def preprocess(raw):
+    dataDict = []
+    names = map(lambda x:x[1:-1], raw.pop(0))
+    for row in raw:
+        d = {}; i = 0
+        for name in names:
+            d[name] = row[i]
+            if (i == 7) or (i == 8):
+                d[name] = d[name][1:-1]
+            elif i != 2:
+                d[name] = int(d[name])
+            i += 1
+        dataDict.append(dict(d))
+
+    print "Preprocessing Done!"
+    return dataDict
+
+### Read train data
+rawData = readCSV('train.csv')
+data = preprocess(rawData)
+
+### global average
+globalAverage = 1.0 * sum(map(lambda x:x['Sales'], data)) / len(data)
+print globalAverage
+
+def preprocessTestData(test):
+    dataDict = []
+    names = map(lambda x:x[1:-1], test.pop(0))
+    for row in test:
+        d = {}; i = 0
+        for name in names:
+            d[name] = row[i]
+            if (i == 6) or (i == 7):
+                d[name] = d[name][1:-1]
+            elif i != 3:
+                d[name] = int(d[name])
+            i += 1
+        dataDict.append(dict(d))
+
+    print "Test Data is ready!"
+    return dataDict
+
+### Read test data
+rawTestData = readCSV('test.csv')
+testData = preprocessTestData(rawTestData)
+
+def writeCSV(filename, prediction):
+    writer = open(filename,'w')
+    writer.write('"Id","Sales"' + '\n')
+    for i in range(len(prediction)):
+        writer.write(str(i+1)+','+str(prediction[i]) + '\n')
+    writer.close()
+    print "Finished writing!"
+
+### Write the answer
+prediction = [globalAverage for i in range(len(testData))]
+writeCSV('predict.csv', prediction)
+
